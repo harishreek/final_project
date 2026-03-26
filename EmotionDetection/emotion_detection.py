e4abb1361fed4d3a59fc9f7e6528f92293923bfa
harishreek <harishreekarthik@gmai.com>
2026-03-26 14:00:51 -0400

    task 6

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze:str):
+   url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+   headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+   input_text = { "raw_document": { "text": text_to_analyze} }
+   response = requests.post(url=url, json = input_text, headers = headers)
+   formatted_response = json.loads(response.text)
+   emotions = formatted_response['emotionPredictions'][0]['emotion']
+   dominant_emotion = max(emotions, key=emotions.get)
+   emotions['dominant_emotion'] = dominant_emotion
+   return emotions 
+
+
+
+
+#from emotion_detection import emotion_detector
+#from EmotionDetection.emotion_detection import emotion_detector
+#emotion_detector("I am so happy I am doing this.")
